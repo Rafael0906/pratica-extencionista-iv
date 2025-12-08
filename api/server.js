b3979910b7e75b8aefc91715a990b3c8eb44c3ca
Rafael0906 <rafa.olivier.205@gmail.com>
2025-12-08 19:51:04 -0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,1 +1,17 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Rota principal
+app.get("/", (req, res) => {
+    res.send({
+        status: "online",
+        mensagem: "API da Prática Extensionista IV rodando com sucesso!"
+    });
+});
+
+// Inicia o servidor
+app.listen(PORT, () => {
+    console.log("Servidor iniciado na porta " + PORT);
+});
 
